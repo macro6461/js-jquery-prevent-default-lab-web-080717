4d83aff633053401350433a49458605073bbae5c
macro6461 <mattcroak718@gmail.com>
2017-09-25 15:20:38 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm(){
+$('form').on('submit', function(){
+  var item = $('#item').val()
+
+  $('ol').append('<li>' + item + '</li>' )
+  event.preventDefault()
+})
+}
